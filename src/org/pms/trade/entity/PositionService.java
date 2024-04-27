ae1f46c998c91f6a19cd0310c4c9464464b54476
sheikmca <shaik484@gmail.com>
2024-04-27 16:14:37 +0800

    Update PositionService.java
    Update PositionService.java

--- src/org/pms/trade/entity/PositionService.java
+++ src/org/pms/trade/entity/PositionService.java
@@ -1,4 +1,33 @@
 package org.pms.trade.entity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+@Service
 public class PositionService {
+
+    private final PositionRepository positionRepository;
+
+    @Autowired
+    public PositionService(PositionRepository positionRepository) {
+        this.positionRepository = positionRepository;
+    }
+
+    public PositionEntity savePosition(PositionEntity position) {
+        return positionRepository.save(position);
+    }
+
+    public Optional<PositionEntity> getPositionById(String id) {
+        return positionRepository.findById(id);
+    }
+
+    public List<PositionEntity> getAllPositions() {
+        return positionRepository.findAll();
+    }
+
+    public void deletePositionById(String id) {
+        positionRepository.deleteById(id);
+    }
 }
+
