c52a0fabd0eb8c755f3572289dd57bab52c1a2da
sheikmca <shaik484@gmail.com>
2024-04-27 17:15:18 +0800

    Update PositionServicesImpl.java
    Update PositionServicesImpl.java

--- src/org/pms/trade/api/PositionServicesImpl.java
+++ src/org/pms/trade/api/PositionServicesImpl.java
@@ -34,4 +34,8 @@ public class PositionServicesImpl implements PositionServices {
 
         return ret;
     }
+
+    public List<PositionEntity> getAllPositions() {
+        return positionRepository.findAll();
+    }
 }
