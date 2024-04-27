ced7be7b07ac53dbb7c777db782c0eec705c2762
sheikmca <shaik484@gmail.com>
2024-04-27 14:24:45 +0800

    Update InstrumentService.java
    Update InstrumentService.java

--- src/org/pms/trade/entity/InstrumentService.java
+++ src/org/pms/trade/entity/InstrumentService.java
@@ -1,4 +1,36 @@
 package org.pms.trade.entity;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class InstrumentService {
+
+ private final InstrumentRepository instrumentRepository;
+
+    @Autowired
+    public InstrumentService(InstrumentRepository instrumentRepository) {
+        this.instrumentRepository = instrumentRepository;
+    }
+
+    public InstrumentEntity saveInstrument(InstrumentEntity instrument) {
+        return instrumentRepository.save(instrument);
+    }
+
+    public Optional<InstrumentEntity> getInstrumentById(String instrumentId) {
+        return instrumentRepository.findById(instrumentId);
+    }
+
+    public List<InstrumentEntity> getAllInstruments() {
+        return instrumentRepository.findAll();
+    }
+
+    public void deleteInstrumentById(String instrumentId) {
+        instrumentRepository.deleteById(instrumentId);
+    }
+  
 }
