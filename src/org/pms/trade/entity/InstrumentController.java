c332dfe92f040eab866f2e2745142e3e844e5445
sheikmca <shaik484@gmail.com>
2024-04-27 13:57:27 +0800

    Created an InstrumentController with required implementation

---
+++ src/org/pms/trade/entity/InstrumentController.java
@@ -0,0 +1,79 @@
+// InstrumentService.java
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InstrumentService {
+
+    private final InstrumentRepository instrumentRepository;
+
+    @Autowired
+    public InstrumentService(InstrumentRepository instrumentRepository) {
+        this.instrumentRepository = instrumentRepository;
+    }
+
+    public InstrumentEntity saveInstrument(InstrumentEntity instrument) {
+        return instrumentRepository.save(instrument);
+    }
+
+    public Optional<InstrumentEntity> getInstrumentById(String instrumentId) {
+        return instrumentRepository.findById(instrumentId);
+    }
+
+    public List<InstrumentEntity> getAllInstruments() {
+        return instrumentRepository.findAll();
+    }
+
+    public void deleteInstrumentById(String instrumentId) {
+        instrumentRepository.deleteById(instrumentId);
+    }
+}
+
+
+
+// InstrumentController.java
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/instruments")
+public class InstrumentController {
+
+    private final InstrumentService instrumentService;
+
+    @Autowired
+    public InstrumentController(InstrumentService instrumentService) {
+        this.instrumentService = instrumentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<InstrumentEntity> createInstrument(@RequestBody InstrumentEntity instrument) {
+        InstrumentEntity savedInstrument = instrumentService.saveInstrument(instrument);
+        return ResponseEntity.ok(savedInstrument);
+    }
+
+    @GetMapping("/{instrumentId}")
+    public ResponseEntity<InstrumentEntity> getInstrumentById(@PathVariable String instrumentId) {
+        return instrumentService.getInstrumentById(instrumentId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<InstrumentEntity>> getAllInstruments() {
+        List<InstrumentEntity> instruments = instrumentService.getAllInstruments();
+        return ResponseEntity.ok(instruments);
+    }
+
+    @DeleteMapping("/{instrumentId}")
+    public ResponseEntity<Void> deleteInstrumentById(@PathVariable String instrumentId) {
+        instrumentService.deleteInstrumentById(instrumentId);
+        return ResponseEntity.noContent().build();
+    }
+}
