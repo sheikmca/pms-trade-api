f897dca295eb99ca4cca08126cdcacf419d052ef
Trung Thang Le <le_trung_thang@nsearchglobal.com>
2024-04-27 12:15:29 +0800

    DAO class 2

--- src/org/pms/trade/entity/Users.java
+++ src/org/pms/trade/entity/Users.java
@@ -1,4 +1,27 @@
 package org.pms.trade.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Users {
+    @Id
+    private String email;
+    private String name;
+    private String password;
+
+
 }
