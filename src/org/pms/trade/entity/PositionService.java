2034171ddb143c5f409a31cd7135e31516de0f9a
Trung Thang Le <le_trung_thang@nsearchglobal.com>
2024-04-27 16:26:06 +0800

    PositionRepository 2

--- src/org/pms/trade/entity/PositionService.java
+++ src/org/pms/trade/entity/PositionService.java
@@ -1,4 +1,16 @@
 package org.pms.trade.entity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PositionService {
+    private final PositionRepository repository;
+
+    @Autowired
+    public PositionService(PositionRepository repo) {
+        this.repository = repo;
+
+    }
+
 }
