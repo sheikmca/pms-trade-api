797ba9a04a728981bc9ba437107ffb6f98d6a480
sheikmca <shaik484@gmail.com>
2024-04-27 14:01:10 +0800

    Provide necessary implementation function for Instrument service

--- src/org/pms/trade/entity/InstrumentService.java
+++ src/org/pms/trade/entity/InstrumentService.java
@@ -1,4 +1,29 @@
 package org.pms.trade.entity;
 
 public class InstrumentService {
+
+    private final InstrumentRepository instrumentRepository;
+
+    @Autowired
+    public InstrumentService(InstrumentRepository instrumentRepository) {
+        this.instrumentRepository = instrumentRepository;
+    }
+
+    public InstrumentEntity saveInstrument(InstrumentEntity instrument) {
+        return instrumentRepository.save(instrument);
+    }
+
+    public Optional<InstrumentEntity> getInstrumentById(String instrumentId) {
+        return instrumentRepository.findById(instrumentId);
+    }
+
+    public List<InstrumentEntity> getAllInstruments() {
+        return instrumentRepository.findAll();
+    }
+
+    public void deleteInstrumentById(String instrumentId) {
+        instrumentRepository.deleteById(instrumentId);
+    }
+
+
 }
