b4298e5d2d6564e73e90d0195a5fb68a68489a4b
Trung Thang Le <le_trung_thang@nsearchglobal.com>
2024-04-27 12:29:49 +0800

    entity

--- src/org/pms/trade/entity/PositionEntity.java
+++ src/org/pms/trade/entity/PositionEntity.java
@@ -1,4 +1,31 @@
 package org.pms.trade.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "position")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class PositionEntity {
-}
+    @Id
+    private String id;
+    private String transaction_ref;
+    private String instrument_Id;
+    private String trade_type;
+    private Integer unit;
+    private Double trade_value;
+
+
+}// swagger endpoint => return mock portfolio data via swagger in ur local => i will pull ur code => run in my local => public to
