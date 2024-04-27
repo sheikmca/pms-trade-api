2b971226e2b66860a0504060597fe764cbeab574
Trung Thang Le <le_trung_thang@nsearchglobal.com>
2024-04-27 12:09:46 +0800

    entity class

--- src/org/pms/trade/entity/InstrumentEntity.java
+++ src/org/pms/trade/entity/InstrumentEntity.java
@@ -1,4 +1,27 @@
 package org.pms.trade.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "instrument")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class InstrumentEntity {
+    @Id
+    private String  instrumentId;
+    private BigDecimal instrumentValue;
+    private String instrumentName;
+    private String instrumentType;
+
 }
